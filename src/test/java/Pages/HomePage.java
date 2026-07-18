ad5c728bbf80391f23682a0e3fa5171eadefa2da
Modise625 <modisesekati9@gmail.com>
2026-07-18 18:39:25 +0200

    Created a method to verify homepage title

--- src/test/java/Pages/HomePage.java
+++ src/test/java/Pages/HomePage.java
@@ -1,4 +1,24 @@
 package Pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class HomePage {
+
+    WebDriver driver;
+    @FindBy(xpath = "//*[@id=\"overview-instructor\"]/img")
+    WebElement instructorImage_xpath;
+
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+
+    }
+
+    public void verifyInstructorImageIsDisplayed() {
+        instructorImage_xpath.isDisplayed();
+    }
+
+
 }
